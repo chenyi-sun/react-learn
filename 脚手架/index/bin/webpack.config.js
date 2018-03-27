4db57b01ead7a66e00adbc7960c7a1ef0d94b544
/ </>
2018-03-27 13:39:21 +0800

    approve

---
+++ 脚手架/index/bin/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+    entry: path.resolve(__dirname, '../src/index.js'),
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015', 'react'],
+                    }
+                },
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
