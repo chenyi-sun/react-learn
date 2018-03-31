88473ec66b9f4b19113fcdba97b8c042439c3599
/ </>
2018-03-31 13:16:12 +0800

    improve

--- learn2/my-app/src/index.js
+++ learn2/my-app/src/index.js
@@ -3,26 +3,62 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
+function Formated(props){
+    return (
+    <div>
+        <p>it is {props.data.toLocaleTimeString()}</p>
+        <p>{props.name}</p>
+    </div>
+    )
+}
+
 
 class Clock extends React.Component {
     constructor(props){
-        supoer(props);
-        this.state = {date: new Date()};
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
+    componentDidMount(){
+        this.timerDI = setInterval(
+            ()=> this.tick(),
+            1000
+        );
+    }
+    componentWillUnmount(){
+        clearInterval(this.timerDI);
+    }
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+    activeLaser(e){
+        function handleClick(e){
+            
+        }
     }
     render() {
         return (
             <div>
                 <h1>
                     it is {this.state.date.toLocaleTimeString()}
+                    <Formated name={ks} data= {this.state.date}/>
+                    <a href="#" onClick={this.activeLaser}>center</a>
                 </h1>
             </div>
         )
     }
 }
 
-
+let ks = "sss";
 
 ReactDOM.render(
-    <Clock/ >,
+    (
+    <div>
+        <Clock  />
+    </div>
+    ),
     document.getElementById('root')
 );
